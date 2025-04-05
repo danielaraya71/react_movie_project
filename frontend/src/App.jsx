4e127d01ceac5d8a5873c315702a5d6d8db89c10
Daniel Araya <39106936+danielaraya71@users.noreply.github.com>
2025-04-05 16:25:45 -0600

    Added css files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import './App.css'
+import './css/App.css'
 import Home from './pages/Home'
 import MovieCard from './components/MovieCard'
+import { Routes, Route} from 'react-router-dom'
+import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <>
-      <div>
-        <Home />
-      </div>
-    </>
+    <div>
+      <NavBar />
+    <main className='mai-content'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import "../css/Favorites.css"
+
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>No favorite movies</h2>
+        <p>Add movies to your favorites list</p>
+    </div>
+}
+
+export default Favorites
